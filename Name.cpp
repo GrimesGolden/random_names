652f2672a9ea26aecec731ece591e0c0e15e8b8e
Jordan Tobin <jordantobin123@hotmail.com>
2023-08-22 17:42:18 -0700

    Last name for now.

--- Name.cpp
+++ Name.cpp
@@ -12,7 +12,7 @@ void name(int total) {
 	int f_index = 0;
 	int l_index = 0;
 	// Vectors to store name sections.
-	vector<string> first = {"Danny", "Virgil", "Johnny", "Lil", "ASAP", "Ratchet", "Piper", "Master", "Vector", "Jackson", "Shootie", "Big", "Lieutenant", "Commander", "Froggie", "Pimp", "Mr.", "Merlot", "Fattie", "Shawn", "Slaine", "HomeBoy", "Prozac", "Goats", "Dirt", "Young", "Freddy", "Ridgeback", "Dollarbill", "Tyler", "Sadboy", "Conner", "Maxo", "Dante", "Tyrone", "Taylor", "Doug", "Slapface", "Susboy", "Joe"};
+	vector<string> first = {"Danny", "Virgil", "Johnny", "Lil", "ASAP", "Ratchet", "Piper", "Master", "Vector", "Jackson", "Shootie", "Big", "Lieutenant", "Commander", "Froggie", "Pimp", "Mr.", "Merlot", "Fattie", "Shawn", "Slaine", "HomeBoy", "Prozac", "Goats", "Dirt", "Young", "Freddy", "Ridgeback", "Dollarbill", "Tyler", "Sadboy", "Conner", "Maxo", "Dante", "Tyrone", "Taylor", "Doug", "Slapface", "Susboy", "Joe", "Oldboy"};
 	vector<string> last = { "TwoDicks", "FastHands", "McKenzie", "Splice", "FaceSlap", "Harambe", "TwoHands", "McGee", "McBride", "TwoShort", "TwoChains", "Shephard", "BackHand", "StealYoGurl", "Leather", "LostHisShoe", "HighIQ", "Wigs", "White", "Sandman", "Turner", "Milkbone", "Nasty", "Papoose", "Jackson", "Countbills", "McGregor", "FatPocks", "Kream", "LayPipe", "Brown", "Swift", "Manchester", "2000", "Rogan"};
 	// The "random" name itself.
 	string name;
